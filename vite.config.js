5bdb57ad979e585268a80b07a0b79fdf5873a526
Rupesh <srdncomputers@gmail.com>
2026-04-13 16:28:52 +0530

    fix: correct vite.config import - use @sveltejs/kit/vite not vite-plugin-svelte

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
-import { sveltekit } from '@sveltejs/vite-plugin-svelte';
+import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+  plugins: [sveltekit()]
 });
